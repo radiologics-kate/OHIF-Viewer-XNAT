c7ba5ad4fe19db166b5b7a365929615f33ff2c1c
James Petts <jpetts@12136phydt.icr.ac.uk>
2018-11-02 14:50:09 +0000

    Pull out all the images in the stack and build a multiframe to feed to the SEG generator.

--- Packages/icr-xnat-roi/client/lib/classes/DICOMSEGWriter.js
+++ Packages/icr-xnat-roi/client/lib/classes/DICOMSEGWriter.js
@@ -1,4 +1,4 @@
-import { cornerstoneTools } from 'meteor/ohif:cornerstone';
+import { cornerstone, cornerstoneTools } from 'meteor/ohif:cornerstone';
 import { OHIF } from 'meteor/ohif:core';
 
 const modules = cornerstoneTools.store.modules;
@@ -14,10 +14,54 @@ export class DICOMSEGWriter {
     console.log(seriesInfo);
   }
 
+
+  // TODO Check if multiframe!
+  // TODO Then grab only one dataset and call dcmjs.normalizers.Normalizer.normalizeToDataset([dataset]);
   writeDICOMSeg (masks) {
     const bitArrays = [];
 
-    console.log(masks);
+    // Grab the base image DICOM.
+    const activeEnabledElement = OHIF.viewerbase.viewportUtils.getEnabledElementForActiveElement();
+    const element = activeEnabledElement.element;
+
+    const stackToolState = cornerstoneTools.getToolState(element, 'stack');
+    const imageIds = stackToolState.data[0].imageIds;
+
+
+    console.log(imageIds);
+
+    let imagePromises = [];
+    for (let i = 0; i < imageIds.length; i++) {
+      imagePromises.push(
+        cornerstone.loadImage(imageIds[0])
+      );
+    }
+
+    Promise.all(imagePromises).then((images) => {
+      console.log(images);
+
+      const datasets = [];
+
+      for (let i = 0; i < images.length; i++) {
+        const image = images[i];
+        const arrayBuffer = image.data.byteArray.buffer;
+        const dicomData = dcmjs.data.DicomMessage.readFile(arrayBuffer);
+        const dataset = dcmjs.data.DicomMetaDictionary.naturalizeDataset(dicomData.dict);
+
+        dataset._meta = dcmjs.data.DicomMetaDictionary.namifyDataset(dicomData.meta);
+
+        datasets.push(dataset);
+      }
+
+      console.log(datasets);
+
+      const multiframe = dcmjs.normalizers.Normalizer.normalizeToDataset(datasets);
+
+      console.log(multiframe);
+
+    });
+
+    return;
 
     console.log(dcmjs.data.BitArray);
 
