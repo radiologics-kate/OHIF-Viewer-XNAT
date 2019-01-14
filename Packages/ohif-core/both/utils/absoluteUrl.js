8ece150791b30bab25ac92e3767659fb01577417
James Petts <jpetts@12136phydt.icr.ac.uk>
2019-01-14 11:32:21 +0000

    Remove measurement table for now, as it currently isn't finished/well integrated.

--- Packages/ohif-core/both/utils/absoluteUrl.js
+++ Packages/ohif-core/both/utils/absoluteUrl.js
@@ -1,37 +1,37 @@
-import { OHIF } from 'meteor/ohif:core';
+import { OHIF } from "meteor/ohif:core";
 
 // Return an absolute URL with the page domain using sub path of ROOT_URL
 // to let multiple domains directed to the same server work
 
-const routingMode = 'XNAT';
+const routingMode = "XNAT";
 
-if (routingMode === 'XNAT') {
+if (routingMode === "XNAT") {
   OHIF.utils.absoluteUrl = function(path) {
     // JPETTS -- Overriding this function in order to display correctly when hosted at an arbitrary subdirectory in XNAT
-    let viewerUrl = Session.get('viewerRoot');
+    let viewerUrl = Session.get("viewerRoot");
 
-    if (path[0] === '/') {
+    if (path[0] === "/") {
       return `${viewerUrl}${path}`;
     }
 
     return `${viewerUrl}/${path}`;
   };
-} else if (routingMode === 'LOCALHOST') {
+} else if (routingMode === "LOCALHOST") {
   OHIF.utils.absoluteUrl = function(path) {
-      let absolutePath = '/';
+    let absolutePath = "/";
 
-      const absoluteUrl = Meteor.absoluteUrl();
-      const absoluteUrlParts = absoluteUrl.split('/');
+    const absoluteUrl = Meteor.absoluteUrl();
+    const absoluteUrlParts = absoluteUrl.split("/");
 
-      if (absoluteUrlParts.length > 4) {
-          const rootUrlPrefixIndex = absoluteUrl.indexOf(absoluteUrlParts[3]);
-          absolutePath += absoluteUrl.substring(rootUrlPrefixIndex) + path;
-      } else {
-          absolutePath += path;
-      }
+    if (absoluteUrlParts.length > 4) {
+      const rootUrlPrefixIndex = absoluteUrl.indexOf(absoluteUrlParts[3]);
+      absolutePath += absoluteUrl.substring(rootUrlPrefixIndex) + path;
+    } else {
+      absolutePath += path;
+    }
 
-      return absolutePath.replace(/\/\/+/g, '/');
+    return absolutePath.replace(/\/\/+/g, "/");
   };
 } else {
-  console.error('routingMode set to an invalid value.');
+  console.error("routingMode set to an invalid value.");
 }
