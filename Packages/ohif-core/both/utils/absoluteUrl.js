659718e675a47e2f2439914e1c7a38f51a153d57
James Petts <jpetts@12136phydt.icr.ac.uk>
2019-01-18 16:26:53 +0000

    Can at least save NIFTI back to SEG. Some issue saving new SEGs that I need to look into.

--- Packages/ohif-core/both/utils/absoluteUrl.js
+++ Packages/ohif-core/both/utils/absoluteUrl.js
@@ -3,7 +3,7 @@ import { OHIF } from "meteor/ohif:core";
 // Return an absolute URL with the page domain using sub path of ROOT_URL
 // to let multiple domains directed to the same server work
 
-const routingMode = "LOCALHOST";
+const routingMode = "XNAT";
 
 if (routingMode === "XNAT") {
   OHIF.utils.absoluteUrl = function(path) {
